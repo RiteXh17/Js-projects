90f18700e7ef7691e1792168123d10b030ae5ce1
RiteXh <riteXh17@gmail.com>
2024-10-24 11:23:33 +0530

    Added one more project of js as Qr code Generator

---
+++ QR Code generator/script.js
@@ -0,0 +1,20 @@
+let imgBox = document.getElementById("imgBox")
+let img = document.getElementById("qrImg")
+let qrText = document.getElementById("qrText")
+
+
+function generateQr(){
+    if(qrText.value.length>0){
+        img.src = " https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+
+        imgBox.classList.add("show-img")
+    }
+    else{
+        qrText.classList.add("error")
+        setTimeout(() => {
+        qrText.classList.remove("error")
+
+        }, 1000);
+    }
+}
+
