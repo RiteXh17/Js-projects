326cd52674736e4c2407e3c1bfac49acabc0fcb6
RiteXh <riteXh17@gmail.com>
2024-10-24 12:00:21 +0530

    Your commit message

--- DOM Project/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ DOM Project/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a83d682a89e213af538de5675505683398c03961")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c64a4198df285e43d2246f18e9992e21270532f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
