cca0e17e326361d4b9549e41db98979ce29eed54
RiteXh <riteXh17@gmail.com>
2024-10-27 10:28:46 +0530

    index file updated

--- DOM Project/.net/1 try/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ DOM Project/.net/1 try/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a83d682a89e213af538de5675505683398c03961")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aac4119937cad49d0a1f31795bedbedc87092624")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
